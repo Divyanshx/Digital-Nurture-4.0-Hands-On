68d5897651e57170e5a9966a426549fde1efdd01
Divyansh <sharmadivyansh651@gmail.com>
2025-07-04 18:05:14 +0530

    week 3 work

---
+++ Week3_Spring_Core_And_Maven/01_Basic_Configuration/code/src/main/java/com/library/App.java
@@ -0,0 +1,19 @@
+package com.library;
+
+import com.library.repository.BookRepository;
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService bookService = context.getBean("bookService", BookService.class);
+        bookService.addBook();
+
+        ookRepository bookRepository = context.getBean("bookRepository", BookRepository.class);
+        bookRepository.saveBook();
+    }
+}
